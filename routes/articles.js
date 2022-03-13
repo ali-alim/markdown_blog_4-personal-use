ab695c8357b524aaff78e1172d648065a942a1ff
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-13 19:45:00 +0400

    create form and article model

---
+++ routes/articles.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const Article = require('../models/article')
+//does the same as app.get()
+const router = express.Router()
+
+router.get('/new', (req,res) => {
+    res.render('articles/new')
+})
+
+router.post('/', (req,res) => {
+
+})
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 const express = require('express');
+const mongoose = require("mongoose")
+const articleRouter = require('./routes/articles')
 const app = express();
+
+mongoose.connect('mongodb://localhost/blog',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    
+    }
+)
 
 //converts all ejs code to html
 app.set('view engine', 'ejs');
 
+
+app.use('/articles', articleRouter)
+
+
+
+
 app.get('/', (req, res) => {
-    res.render("index")
+    const articles = [
+        {
+        title: 'First Article',
+        createdAt: new Date(),
+        description: 'artircle description'
+    },
+    {
+        title: 'Second Article',
+        createdAt: new Date(),
+        description: 'artircle description'
+    },
+
+]
+    res.render("index", {articles: articles})
+    
 })
 
 app.listen(5000);
